ebf1c393ad425296186377c2df66adde3ae45365
Hoang N. Truong <prodai9573@gmail.com>
2021-10-20 00:16:27 +0700

    Update test-flow.py

--- test-flow.py
+++ test-flow.py
@@ -7,12 +7,13 @@ from prefect import Flow
 from prefect.storage import GitHub
 
 flow = Flow(
-    "github-flow",
-    GitHub(
-        repo="org/repo",                           # name of repo
-        path="flows/my_flow.py",                   # location of flow file in repo
-        access_token_secret="GITHUB_ACCESS_TOKEN"  # name of personal access token secret
-    )
+	"hello-flow-2", 
+	storage=GitHub(
+	    repo="hoangperry/prefect-github-storage-test",
+	    path="test-flow.py",
+	    access_token_secret="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"
+	), 
+	run_config=UniversalRun(labels=["mail.kernel.vn"])
 )
 
 @task
@@ -20,12 +21,8 @@ def say_hello():
 	logger = prefect.context.get("logger")
 	logger.info("Hello, Cloud!")
 
-with Flow("hello-flow-2", GitHub(
-        repo="hoangperry/prefect-github-storage-test",                           # name of repo
-        path="test-flow.py",                   # location of flow file in repo
-        access_token_secret="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"  # name of personal access token secret
-    ), run_config=UniversalRun(labels=["mail.kernel.vn"])) as flow:
-	say_hello()
+
+say_hello.run()
 
 # Register the flow under the "tutorial" project
 flow.register(project_name="test")
